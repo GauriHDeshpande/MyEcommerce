f3cc9f45c8cf5d726e7820d2b22b76ca6f3eae33
Gauri Deshpande <gauri.deshpande22@rediffmail.com>
2023-02-07 14:17:42 +0530

    Added product category page

--- admin-dashboard/src/app/store.js
+++ admin-dashboard/src/app/store.js
@@ -3,7 +3,14 @@ import authReducer from '../features/auth/authSlice';
 import customerReducer from '../features/cutomers/customerSlice';
 import productReducer from '../features/product/productSlice';
 import brandReducer from '../features/brand/brandSlice';
+import pcategoryReducer from '../features/pcategory/pcategorySlice'
 
 export const store = configureStore({
-  reducer: {auth: authReducer, customers: customerReducer, product: productReducer, brand: brandReducer},
+  reducer: {
+    auth: authReducer, 
+    customers: customerReducer, 
+    product: productReducer, 
+    brand: brandReducer,
+    pcategory: pcategoryReducer,
+  },
 });
--- admin-dashboard/src/features/pcategory/pcategoryService.js
+++ admin-dashboard/src/features/pcategory/pcategoryService.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import axios from "axios";
+import { base_url } from "../../utils/baseURL";
+import { config } from "../../utils/axiosConfig";
 
-const pcategoryService = () => {
-  return (
-    <div>pcategoryService</div>
-  )
-}
+const getProductCategories = async () => {
+  const response = await axios.get(`${base_url}category/`);
 
-export default pcategoryService
+  return response.data;
+};
+const createCategory = async (category) => {
+  const response = await axios.post(`${base_url}category/`, category, config);
+
+  return response.data;
+};
+
+const getProductCategory = async (id) => {
+  const response = await axios.get(`${base_url}category/${id}`, config);
+
+  return response.data;
+};
+
+const deleteProductCategory = async (id) => {
+  const response = await axios.delete(`${base_url}category/${id}`, config);
+
+  return response.data;
+};
+const updateProductCategory = async (category) => {
+  console.log(category);
+  const response = await axios.put(
+    `${base_url}category/${category.id}`,
+    { title: category.pCatData.title },
+    config
+  );
+
+  return response.data;
+};
+const pCategoryService = {
+  getProductCategories,
+  createCategory,
+  getProductCategory,
+  deleteProductCategory,
+  updateProductCategory,
+};
+
+export default pCategoryService;
